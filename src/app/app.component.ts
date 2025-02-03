878df904d441217e887493301b5ffaa39d8d6d86
MoElsherif26 <m.elsherif2026@gmail.com>
2025-02-03 21:36:41 +0200

    prepared project to work

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { initFlowbite } from 'flowbite';
+import { FlowbiteService } from './core/services/flowbite.service';
+import { BrandsComponent } from "./pages/brands/brands.component";
+
 
 @Component({
   selector: 'app-root',
@@ -7,6 +11,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ecommerce-full';
+  constructor(private flowbiteService: FlowbiteService) {}
+  ngOnInit(): void {
+    initFlowbite();
+    this.flowbiteService.loadFlowbite(flowbite => {
+      // Your custom code here
+      // console.log('Flowbite loaded', flowbite);
+    });
+  }
 }
