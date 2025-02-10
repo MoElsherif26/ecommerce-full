99a40f7214df2be2dd8f4889a85654e7f14ee646
MoElsherif26 <m.elsherif2026@gmail.com>
2025-02-10 20:21:18 +0200

    Added Register and Login pages

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,11 +1,52 @@
+import { AuthService } from './../../core/services/auth.service';
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+
+
+  isLoading: boolean = false;
+  errorMessage: string = '';
+
+  // prepare reactive form for register
+  loginForm: FormGroup = new FormGroup({
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+  });
+
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  submit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm);
+      this.isLoading = true;
+      this.authService.login(this.loginForm.value).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.isLoading = false;
+          // navigate to login
+          this.router.navigate(['/home']);
+        },
+        error: (err) => {
+          console.log(err);
+          this.isLoading = false;
+          // display problem message
+          this.errorMessage = err.error.message;
+          
+        }
+      });
+    }
+    else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
 
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,11 +1,65 @@
+import { AuthService } from './../../core/services/auth.service';
 import { Component } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+
+  isLoading: boolean = false;
+  errorMessage: string = '';
+
+  // prepare reactive form for register
+  registerForm: FormGroup = new FormGroup({
+    name: new FormControl(null, [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+    email: new FormControl(null, [Validators.required, Validators.email]),
+    password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    rePassword: new FormControl(null),
+    phone: new FormControl(null, [Validators.required, Validators.pattern(/^01[0125][0-9]{8}$/)])
+  }, this.confirmPassword);
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  confirmPassword(form: AbstractControl) {
+    let password = form.get('password')?.value;
+    let rePassword = form.get('rePassword')?.value;
+    if (password == rePassword) {
+      return null;
+    }
+    else {
+      return {misMatch: true}
+    }
+  }
+
+  submit() {
+    if (this.registerForm.valid) {
+      console.log(this.registerForm);
+      this.isLoading = true;
+      this.authService.register(this.registerForm.value).subscribe({
+        next: (res) => {
+          console.log(res);
+          this.isLoading = false;
+          // navigate to login
+          this.router.navigate(['/login']);
+        },
+        error: (err) => {
+          console.log(err);
+          this.isLoading = false;
+          // display problem message
+          this.errorMessage = err.error.message;
+        }
+      });
+    }
+    else {
+      this.registerForm.markAllAsTouched();
+    }
+  }
+
+
 
 }
