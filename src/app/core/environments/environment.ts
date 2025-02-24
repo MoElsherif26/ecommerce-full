ceefbcb18c61f02e6326edfc1a927b29b36414bf
MoElsherif26 <m.elsherif2026@gmail.com>
2025-02-24 20:25:44 +0200

    modified environments

--- src/app/core/environments/environment.ts
+++ src/app/core/environments/environment.ts
@@ -1,5 +1,4 @@
 export const environment = {
     baseUrl: 'https://ecommerce.routemisr.com',
-    // frontEndUrl: 'http://localhost:4200'
-    frontEndUrl: 'https://fresh-cart-ecommerce-full.vercel.app'
+    frontEndUrl: 'http://localhost:4200'
 }
